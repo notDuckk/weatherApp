bddd4a41b9e1d2a04c19d186acf4941980e07014
notDuck <daylenfrancis908@gmail.com>
2024-11-13 16:47:06 -0500

    deleting some files

--- weather.js
+++ weather.js
@@ -1,6 +1,5 @@
 // const searchZip = document.getElementById('searchZip');
 
-
 const apiKey = 'ef18d390e6563614d70389d312dc8586';
 let city = document.getElementById('city')
 let currentDate = document.getElementById('currentDate')
@@ -8,6 +7,7 @@ let currentTemp = document.getElementById('currentTemp')
 let conditions = document.getElementById('conditions')
 let tempHi = document.getElementById('tempHi')
 let tempLo = document.getElementById('tempLo')
+let body = document.getElementById('body')
 
 const getWeather = () => {
     
@@ -21,6 +21,6 @@ const getWeather = () => {
         conditions.innerHTML = ` ${response.weather[0].main}`,
         tempHi.innerHTML = response.main.temp_min+'°F'+'&nbsp / &nbsp',
         tempLo.innerHTML = response.main.temp_max+'°F'
-        
     });
+
 }
